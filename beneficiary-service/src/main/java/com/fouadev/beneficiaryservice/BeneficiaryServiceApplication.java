5c3fd66294cb328a08ae5b8244aec1eefa389059
FOUAD SAIDI <fouadsai17@gmail.com>
2024-11-17 14:19:18 +0100

    Created gateway-service, discovery-service, config-service, and beneficiary-service

---
+++ beneficiary-service/src/main/java/com/fouadev/beneficiaryservice/BeneficiaryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fouadev.beneficiaryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeneficiaryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeneficiaryServiceApplication.class, args);
+	}
+
+}
